0fd94d67f4d77a46ee4581c36fec3a4eec9a34a2
Graham Tschieder <gtschieder@gmail.com>
2018-11-27 11:26:33 -0500

    prettied up the header a bit

--- Hackstreet_Images/config/routes.rb
+++ Hackstreet_Images/config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   # get '/images', to: 'images#index', as: 'images'
   post '/create', to: 'images#create', as: 'create'
   root 'welcome#index'
+  get 'welcome', to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
