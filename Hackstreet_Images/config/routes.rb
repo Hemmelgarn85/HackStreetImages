57a008f36e82df9aa0723537b500a8458a963a08
Ivan Lavrov <lavrov.ivrr@gmail.com>
2018-11-28 21:39:02 -0500

    Expanded seeds.rb

--- Hackstreet_Images/config/routes.rb
+++ Hackstreet_Images/config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   # resources :images
   # get '/images', to: 'images#index', as: 'images'
   post '/create', to: 'images#create', as: 'create'
-  root 'welcome#index'
+  root 'images#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
