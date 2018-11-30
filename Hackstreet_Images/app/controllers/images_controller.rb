b7ff4491d95ffcc103ac9fffbaee9ef4ee1ad0e2
Graham Tschieder <gtschieder@gmail.com>
2018-11-30 08:16:50 -0500

    put in feed filtering capabilities

--- Hackstreet_Images/app/controllers/images_controller.rb
+++ Hackstreet_Images/app/controllers/images_controller.rb
@@ -4,26 +4,7 @@ class ImagesController < ApplicationController
     images_to_display = Image.all
 
     images_to_display.each do |img|
-      case img.privacy_level
-      when "anon_public"
-        if user_signed_in?
-          images_to_display -= [img] # if the user is signed in, don't show anon pics
-        end
-      when "anon_private"
-        images_to_display -= [img] # never display anon pics
-      when "signed_in_private"
-        if user_signed_in?
-          # we determine if logged in user has permission to see this pic
-          image_poster = User.find(img.user_id)
-          images_to_display -= [img] unless current_user.following? image_poster
-        else
-          images_to_display -= [img] # don't let anon users see private pics of signed in users
-        end
-      when "signed_in_public" 
-        # always display these, uploader designated them as public
-      else
-        # Privacy indeterminable. Just show it.
-      end
+      images_to_display -= [img] unless user_has_view_permission(img, current_user)
     end
     # now that we've determined which images to display, display them
     @images = images_to_display 
@@ -65,6 +46,36 @@ class ImagesController < ApplicationController
     redirect_to root_url
   end
 
+  def user_has_view_permission(img, active_user)
+    should_display = true #presume we should show it, until proven otherwise
+
+    if (!active_user.nil? && active_user.id == img.user_id)
+      return should_display
+    end
+
+    case img.privacy_level
+    when "anon_public"
+      if active_user
+        should_display = false # if the user is signed in, don't show anon pics
+      end
+    when "anon_private"
+      should_display = false # never display anon pics
+    when "signed_in_private"
+      if active_user
+        # we determine if logged in user has permission to see this pic
+        image_poster = User.find(img.user_id)
+        should_display = false unless current_user.following? image_poster
+      else
+        should_display = false # don't let anon users see private pics of signed in users
+      end
+    when "signed_in_public" 
+      # always display these, uploader designated them as public
+    else
+      # Privacy indeterminable. Just show it.
+    end
+
+    should_display
+  end
 
   private
     def user_post_params
--- Hackstreet_Images/app/helpers/images_helper.rb
+++ Hackstreet_Images/app/helpers/images_helper.rb
@@ -6,5 +6,36 @@ module ImagesHelper
         else
           'defaultuser.jpg'
         end
+    end 
+
+    def user_has_view_permission(img, active_user)
+      should_display = true #presume we should show it, until proven otherwise
+  
+      if (!active_user.nil? && active_user.id == img.user_id)
+        return should_display
+      end
+  
+      case img.privacy_level
+      when "anon_public"
+        if active_user
+          should_display = false # if the user is signed in, don't show anon pics
+        end
+      when "anon_private"
+        should_display = false # never display anon pics
+      when "signed_in_private"
+        if active_user
+          # we determine if logged in user has permission to see this pic
+          image_poster = User.find(img.user_id)
+          should_display = false unless current_user.following? image_poster
+        else
+          should_display = false # don't let anon users see private pics of signed in users
+        end
+      when "signed_in_public" 
+        # always display these, uploader designated them as public
+      else
+        # Privacy indeterminable. Just show it.
+      end
+  
+      should_display
     end
 end
