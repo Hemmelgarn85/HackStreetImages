289ed136e1d0c141c6d4df9f12d575f1b4c7ac5b
hemmelgarn85 <hemmelgarn.85@osu.edu>
2018-11-29 21:23:23 -0500

    Updated search to also search for images

--- Hackstreet_Images/app/controllers/search_controller.rb
+++ Hackstreet_Images/app/controllers/search_controller.rb
@@ -6,11 +6,13 @@ class SearchController < ApplicationController
   #created by Michael Hemmelgarn
   def search
     @users = User.ransack(username_cont: params[:q]).result(distinct: true)
+    @images = Image.ransack(name_cont: params[:q]).result(distinct: true)
 
     respond_to do |format|
       format.html {}
       format.json {
         @users = @users.limit(5)
+        @images = @images.limit(5)
       }
     end
   end
--- Hackstreet_Images/app/views/search/search.json.jbuilder
+++ Hackstreet_Images/app/views/search/search.json.jbuilder
@@ -5,3 +5,9 @@ json.users do
     json.url user_path(user)
   end
 end
+json.images do
+  json.array!(@images) do |image|
+    json.name image.name
+    json.url image_path(image)
+  end
+end
