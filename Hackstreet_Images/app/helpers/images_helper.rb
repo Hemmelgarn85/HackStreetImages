09bc245c72e4d0901c9f54731cf49350b6fbb95c
jsoat <soat.3@osu.edu>
2018-11-30 01:08:55 -0500

    Updated avatars and working profile link

--- Hackstreet_Images/app/helpers/images_helper.rb
+++ Hackstreet_Images/app/helpers/images_helper.rb
@@ -1,2 +1,9 @@
 module ImagesHelper
+    def display_avatar(user)
+        if user.profile_image_datafile.attached?
+          user.profile_image_datafile.variant(resize: '150x150x')
+        else
+          'defaultuser.png'
+        end
+    end
 end
