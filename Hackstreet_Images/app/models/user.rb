c1f1beb3ebc29caff7d610c7255f38ea24a0a748
Graham Tschieder <gtschieder@gmail.com>
2018-11-30 04:26:00 -0500

    added min length to username

--- Hackstreet_Images/app/models/user.rb
+++ Hackstreet_Images/app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, uniqueness: true       
+  validates :username, uniqueness: true
+  validates_length_of :username, :minimum => 3       
   has_one_attached :profile_image_datafile
   has_many :images, dependent: :destroy # if the use is deleted, then delete all associated images and comments
   has_many :comments, dependent: :destroy
